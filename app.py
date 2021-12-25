b06cae49921b4f124c5359db98c3c15f15d10192
joonghyunlee <jhlee3141592@gmail.com>
2021-12-26 04:58:34 +0900

    fix to show a drop-down menu

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import configparser
+import json
+import os
+
+import yaml
 from flask import Flask, request, jsonify
 
 from objects import message
@@ -10,12 +15,43 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
-@app.route("/commands", methods=["POST"])
+@app.route("/servers", methods=["POST"])
 def command():
     print(request.get_json())
-    resp = message.Response()
+    resp = message.Response(response_type='inChannel', text='TEST')
+
+    options = [
+        message.Option('KR1', 'KR1'),
+        message.Option('KR2', 'KR2'),
+        message.Option('JP1', 'JP1'),
+        message.Option('US1', 'US1'),
+    ]
+    action = message.ActionAttachment(name='Region',
+                                      text="Select a Region",
+                                      action_type='select',
+                                      options=options)
+    resp.attachments = [message.Attachment(actions=[action])]
+    print(resp.to_dict())
+
+    return jsonify(resp.to_dict())
+
+
+@app.route("/callback", methods=["POST"])
+def webhook():
+    print(request.get_json())
+    cb = message.Callback.from_dict(request.get_json())
+    resp = message.Response(response_type='inChannel', text=cb.action_value)
     return jsonify(resp.to_dict())
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    config_file = os.path.join(os.path.dirname(__file__), '.', os.getenv("CONFIG_FILE"))
+    parser = configparser.ConfigParser()
+    parser.read(config_file)
+
+    print(parser.items('KR1:ALPHA'))
+    print(type(parser.items('KR1:ALPHA')))
+    print(parser.sections())
+
+    app.run(host="0.0.0.0", port=8080)
+
--- objects/message.py
+++ objects/message.py
@@ -1,33 +1,48 @@
-import json
+class Option:
+    def __init__(self, text=None, value=None):
+        self.text = text
+        self.value = value
+
+    def to_dict(self):
+        return {'text': self.text, 'value': self.value}
 
 
 class Attachment:
     def __init__(self,
                  callback_id=None,
-                 actions=None,
-                 name=None,
-                 text=None,
-                 attachment_type=None,
-                 options=None):
+                 actions=None):
         self.callback_id = callback_id
         self.actions = actions
+
+    def to_dict(self):
+        return {'callbackId': self.callback_id,
+                'actions': [action.to_dict() for action in self.actions]}
+
+
+class ActionAttachment(object):
+    def __init__(self, name=None, text=None, action_type=None, options=None):
         self.name = name
         self.text = text
-        self.type = attachment_type
-        self.options = options
+        self.type = action_type
+        self.options = options if options else []
 
-    def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__)
+    def to_dict(self):
+        return {'name': self.name,
+                'text': self.text,
+                'type': self.type,
+                'options': [option.to_dict() for option in self.options]}
 
 
 class Response:
-    def __init__(self):
-        self.response_type = 'inChannel'
-        self.text = 'Hello World'
+    def __init__(self, response_type=None, text=None, attachments=None):
+        self.response_type = response_type
+        self.text = text
+        self.attachments = attachments if attachments else []
 
     def to_dict(self):
         return {'responseType': self.response_type,
-                'text': self.text}
+                'text': self.text,
+                'attachments': [attachment.to_dict() for attachment in self.attachments]}
 
 
 class Request(object):
@@ -37,9 +52,35 @@ class Request(object):
         self.channel_id = ""
         self.channel_name = ""
         self.user_id = ""
+        self.user_name = ""
         self.command = ""
         self.text = ""
         self.response_url = ""
         self.app_token = ""
         self.cmd_token = ""
         self.trigger_id = ""
+
+
+class Callback(object):
+    def __init__(self, command=None,
+                 args=None,
+                 user_id=None,
+                 user_name=None,
+                 action_name=None,
+                 action_value=None):
+        self.command = command
+        self.args = args
+        self.user_id = user_id
+        self.user_name = user_name
+        self.action_name = action_name
+        self.action_value = action_value
+
+    @classmethod
+    def from_dict(cls, value):
+        return Callback(command=value.get('command'),
+                        args=value.get('text'),
+                        user_id=value.get('user').get("id") if 'user' in value else None,
+                        user_name=value.get('user').get("name") if 'user' in value else None,
+                        action_name=value.get('actionName'),
+                        action_value=value.get('actionValue'))
+
